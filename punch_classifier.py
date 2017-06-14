4e2632d27a3916ae24a547419e19172b3dff2728
Beondel <bengmacmillan@gmail.com>
2017-06-14 18:22:36 -0400

    hook and jab prediction added

--- punch_classifier.py
+++ punch_classifier.py
@@ -6,8 +6,10 @@ import sklearn.neighbors as knn
 import matplotlib.pyplot as plt
 import sklearn.metrics as mtr
 
-# import data
+# import and format data
 df = pd.read_csv('./punch_data_1.csv')
+punch_types = ['jab', 'hook']
+df['punch_type'] = df['type'].map(lambda x: punch_types[x])
 
 # separate into training and testing sets
 df_train = df.drop(df.index[[0, 10, 20, 30, 40, 50, 60, 70]])
@@ -15,9 +17,9 @@ df_test = df.loc[[0, 10, 20, 30, 40, 50, 60, 70]]
 
 # separate into features and labels
 training_features = np.array(df_train[['x_acc', 'y_acc', 'z_acc']])
-training_labels = np.array(df_train['type'])
+training_labels = np.array(df_train['punch_type'])
 test_features = np.array(df_test[['x_acc', 'y_acc', 'z_acc']])
-test_labels = np.array(df_test['type'])
+test_labels = np.array(df_test['punch_type'])
 
 # create models
 clf_1 = svm.SVC()
